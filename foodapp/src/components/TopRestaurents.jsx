0a226116f0197bf0f56407841f3c7f9ec786070e
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-01-03 16:44:23 +0530

    card submissions

--- foodapp/src/components/TopRestaurents.jsx
+++ foodapp/src/components/TopRestaurents.jsx
@@ -1,37 +1,32 @@
 import React from "react";
-import { Cloudinary_URL } from "../utils/Constants";
+import CardsRestaurents from "./CardsRestaurents";
 
 const TopRestaurents = ({ data }) => {
+  console.log(data);
   return (
-    <div className="flex h-[40vw] w-[80vw] my-10">
-      <h1 className="pr-5 font-extrabold text-2xl">
-        Top restaurant chains in Delhi
-      </h1>
-      {/* {data.map((item) => {
-        return ( */}
-      <div class="relative flex h-auto flex-col my-16 w-80">
-        <div class="relative m-2.5 overflow-hidden text-white">
-          <img
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-            // {Cloudinary_URL + item.imageId}
-            alt="card-image"
-            className="rounded-2xl"
-          />
-        </div>
-        <div class="px-4">
-          <h6 class="text-slate-800 text-xl font-semibold">
-            {/* {item.name} */} MacDoLund
-          </h6>
-          <p class="text-slate-600 leading-normal font-light">
-            <i class="fa-solid fa-star text-[#2df505]"></i>
-            Barcelona
-          </p>
-          <p class="text-slate-600 leading-normal font-light">American</p>
-          <p class="text-slate-600 leading-normal font-light">Rajendra Place</p>
-        </div>
+    <div className="mx-auto w-[80vw] pt-7 h-[30vw]">
+      <h2 className="font-extrabold text-2xl">Top restaurant chains in Delhi</h2>
+      <div
+        style={{
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+        }}
+        className="flex overflow-scroll py-6"
+      >
+        {data &&
+          data.map((item) => {
+            return (
+              <CardsRestaurents
+                time={item.info.sla.slaString}
+                area={item.info.areaName}
+                name={item.info.name}
+                rating={item.info.avgRating}
+                img={item.info.cloudinaryImageId}
+                cuisines={item.info.cuisines}
+              />
+            );
+          })}
       </div>
-      {/* );
-      })} */}
     </div>
   );
 };
--- foodapp/src/pages/AllRestaurents.jsx
+++ foodapp/src/pages/AllRestaurents.jsx
@@ -5,19 +5,18 @@ import { API } from "../utils/Constants";
 import TopRestaurents from "../components/TopRestaurents";
 
 const AllRestaurents = () => {
-  const [data, setData] = useState([]);
-  const [data2, setData2] = useState([]);
+  const [apiData, setApiData] = useState();
 
   useEffect(() => {
     const getData = async () => {
       try {
         const response = await fetch(API);
         const data = await response.json();
-        // console.log(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-        setData(data.data.cards[0].card.card.imageGridCards.info);
-        setData2(
+
+        console.log(
           data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
         );
+        setApiData(data);
       } catch (error) {
         console.log(error);
       }
@@ -27,11 +26,25 @@ const AllRestaurents = () => {
 
   return (
     <div className="pt-10 flex flex-col justify-center items-center">
-      <Corousel data={data} />
-
-     
-
-      <TopRestaurents data={data2} />
+      <div>
+        <Corousel
+          data={
+            apiData ? apiData.data.cards[0].card.card.imageGridCards.info : []
+          }
+        />
+      </div>
+      <div className="pb-48">
+        <TopRestaurents
+          className="mx-auto"
+          data={
+            apiData
+              ? apiData.data.cards[1].card.card.gridElements.infoWithStyle
+                  .restaurants
+              : []
+          }
+        />
+          <hr style={{ border: "1px solid #ddd", marginTop: "50px" }} />
+      </div>
     </div>
   );
 };
